15b388685ad52905ade8cecc35ea68bed9f7e423
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-08-02 01:20:45 +0000

    ADM08 - cadastro e edição de usuários - validação

--- src/main/java/br/com/urubatanpacheco/ediaristas/core/repositories/UsuarioRepository.java
+++ src/main/java/br/com/urubatanpacheco/ediaristas/core/repositories/UsuarioRepository.java
@@ -3,10 +3,14 @@ package br.com.urubatanpacheco.ediaristas.core.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.urubatanpacheco.ediaristas.core.models.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
+
+    @Query("SELECT count(*) > 0 FROM Usuario u WHERE u.email = :email and (:id is null or u.id != :id)")
+    Boolean isEmailJaCadastrado(String email, Long id);
     
 }
