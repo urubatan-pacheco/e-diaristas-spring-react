6c23b586f258bb646e5ef481a1bff03f9482d433
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-08-03 18:23:08 +0000

    Alterando configuração inicial do Spring Security para usar a classe deprecated WebSecurityConfigurerAdapter

--- src/main/java/br/com/urubatanpacheco/ediaristas/config/SecurityConfig.java
+++ src/main/java/br/com/urubatanpacheco/ediaristas/config/SecurityConfig.java
@@ -1,23 +1,16 @@
 package br.com.urubatanpacheco.ediaristas.config;
-
-import org.springframework.context.annotation.Bean;
+ 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+ 
 @Configuration
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // http
-        //     .authorizeHttpRequests((authz) -> authz
-        //         .anyRequest().authenticated()
-        //     )
-        //     .httpBasic(withDefaults());
-        http.authorizeRequests().anyRequest().permitAll();
-        return http.build();
-    }
-
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+   @Override
+   protected void configure(HttpSecurity http) throws Exception {
+       http.authorizeRequests().anyRequest().permitAll();
+   }
 }
 
