e5fdc8eaaa9159a9c0ec541021ee9de6edf90f62
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-09-19 23:51:51 +0000

    buscar diaristas - modelo uml

---
+++ src/main/resources/db/migration/V3__criar_tabela_foto.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `foto` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `content_length` BIGINT NOT NULL,
+    `contentType` VARCHAR(255) NOT NULL,
+    `filename` VARCHAR(255) NOT NULL,  
+    `url` VARCHAR(255) NOT NULL,  
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`filename`)
+);
