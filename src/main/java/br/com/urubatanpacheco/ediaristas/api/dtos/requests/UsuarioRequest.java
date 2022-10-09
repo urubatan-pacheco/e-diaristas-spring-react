cbecd4bead8d63752bdc9d48c0e167471db0f48c
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-10-09 02:04:00 +0000

    Contratação de diaristas (dados do cliente) - Criação do DTO e Mapper de usuário

---
+++ src/main/java/br/com/urubatanpacheco/ediaristas/api/dtos/requests/UsuarioRequest.java
@@ -0,0 +1,68 @@
+package br.com.urubatanpacheco.ediaristas.api.dtos.requests;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(SnakeCaseStrategy.class)
+public class UsuarioRequest {
+
+    @NotNull
+    @Size(min = 3, max = 255)
+    private String nomeCompleto;
+
+    @NotNull
+    @Size(max = 255)
+    @Email
+    private String email;
+    
+    @NotNull
+    @NotEmpty
+    private String password;
+
+    @NotNull
+    @NotEmpty    
+    private String passwordConfirmation;
+
+    @NotNull    
+    private Integer tipoUsuario;
+    // depois criaremos uma validação específica
+
+    @NotNull
+    @Size(min = 11, max = 11)
+    @CPF
+    private String cpf;
+    
+    @NotNull
+    @Past
+    @DateTimeFormat(iso = ISO.DATE)
+    private LocalDate nascimento;
+    
+    @NotNull
+    @Size(min = 11, max = 11)
+    private String telefone;
+    
+    // private String fotoDocumento;
+    
+    @Size(min = 11, max = 255)
+    private String chave_pix;
+
+}
---
+++ src/main/java/br/com/urubatanpacheco/ediaristas/api/mappers/ApiUsuarioMapper.java
@@ -0,0 +1,28 @@
+package br.com.urubatanpacheco.ediaristas.api.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import br.com.urubatanpacheco.ediaristas.api.dtos.requests.UsuarioRequest;
+import br.com.urubatanpacheco.ediaristas.core.enums.TipoUsuario;
+import br.com.urubatanpacheco.ediaristas.core.models.Usuario;
+
+import java.util.stream.Stream;
+
+@Mapper(componentModel = "spring")
+public interface ApiUsuarioMapper {
+    ApiUsuarioMapper  INSTANCE =  Mappers.getMapper(ApiUsuarioMapper.class);
+
+    //@Mapping(target = "urlFotoUsuario", source = "fotoUsuario.url")
+    @Mapping(target = "senha", source = "password")
+    Usuario toModel(UsuarioRequest request);
+
+    default TipoUsuario integerToTipoUsuario(Integer valor) {
+        return Stream.of(TipoUsuario.values())
+        .filter(tipoUsuario -> tipoUsuario.getId().equals(valor))
+        .findFirst()
+        .orElseThrow(() -> new IllegalArgumentException("Tipo de usuário inválido"));
+    }
+
+}
