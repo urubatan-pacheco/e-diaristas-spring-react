36cc57fcb9fac7f9b5d683936628ed30662df630
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-10-10 04:27:50 +0000

    Contratação de diárias (dados do cliente) - Criação do método para tratar erros de validação na API

--- src/main/java/br/com/urubatanpacheco/ediaristas/api/handlers/ApiExceptionHandler.java
+++ src/main/java/br/com/urubatanpacheco/ediaristas/api/handlers/ApiExceptionHandler.java
@@ -1,21 +1,52 @@
 package br.com.urubatanpacheco.ediaristas.api.handlers;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 
 import br.com.urubatanpacheco.ediaristas.api.dtos.responses.ErrorResponse;
 import br.com.urubatanpacheco.ediaristas.core.services.consultaEndereco.exceptions.EnderecoServiceException;
 
 @RestControllerAdvice(annotations = RestController.class)
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+    private SnakeCaseStrategy camelCaseToSnakeCase =  new SnakeCaseStrategy();
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException ex,
+            HttpHeaders headers, 
+            HttpStatus status, 
+            WebRequest request) {
+                var body = new HashMap<String, List<String>>();
+
+                ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
+                    var field = camelCaseToSnakeCase.translate(fieldError.getField()) ;
+                    if (!body.containsKey(field)) {
+                        var fieldErrors = new ArrayList<String>();
+                        fieldErrors.add(fieldError.getDefaultMessage());
+                        body.put(field, fieldErrors);
+                    } else {
+                        body.get(field).add(fieldError.getDefaultMessage());
+                    }
+                });
+
+                return ResponseEntity.badRequest().body(body);
+    }
+
     @ExceptionHandler(EnderecoServiceException.class)
     public  ResponseEntity<Object> handleEnderecoServiceException(EnderecoServiceException exception, HttpServletRequest request) {
         var errorResponse = ErrorResponse.builder()
