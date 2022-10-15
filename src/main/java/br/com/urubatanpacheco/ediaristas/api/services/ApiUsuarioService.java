88ad84ee41274e1a4808702c6020eb659657a4c9
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-10-15 02:13:32 +0000

    Contratação de diárias (dados do cliente) - validação de confirmação de senha

--- src/main/java/br/com/urubatanpacheco/ediaristas/api/services/ApiUsuarioService.java
+++ src/main/java/br/com/urubatanpacheco/ediaristas/api/services/ApiUsuarioService.java
@@ -3,10 +3,12 @@ package br.com.urubatanpacheco.ediaristas.api.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.FieldError;
 
 import br.com.urubatanpacheco.ediaristas.api.dtos.requests.UsuarioRequest;
 import br.com.urubatanpacheco.ediaristas.api.dtos.responses.UsuarioResponse;
 import br.com.urubatanpacheco.ediaristas.api.mappers.ApiUsuarioMapper;
+import br.com.urubatanpacheco.ediaristas.core.exceptions.SenhasNaoConferemException;
 import br.com.urubatanpacheco.ediaristas.core.repositories.UsuarioRepository;
 import br.com.urubatanpacheco.ediaristas.core.validators.UsuarioValidator;
 
@@ -26,11 +28,25 @@ public class ApiUsuarioService {
     private UsuarioValidator validator;
 
     public UsuarioResponse cadastrar(UsuarioRequest request) {
+        validarConfirmacaoSenha(request);
+
         var usuarioParaCadastrar = mapper.toModel(request);
 
         validator.validar(usuarioParaCadastrar);
 
         usuarioParaCadastrar.setSenha(passwordEncoder.encode(usuarioParaCadastrar.getSenha()));
         return mapper.toResponse(repository.save(usuarioParaCadastrar));
+    }
+
+    private void validarConfirmacaoSenha(UsuarioRequest obj) {
+
+        var confirmacaoSenha = obj.getPasswordConfirmation();
+        if (obj.getPassword().equals(confirmacaoSenha) == false) {
+            var mensagem = "Os dois campos de senhas não conferem!";
+            var fieldError = new FieldError(obj.getClass().getName(), "passwordConfirmation", confirmacaoSenha, false, null, null, mensagem);
+
+            throw new SenhasNaoConferemException(mensagem, fieldError);
+        }
+
     }
 }
