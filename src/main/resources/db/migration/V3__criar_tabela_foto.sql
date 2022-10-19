77ebdaf7a301bae61db3c1385ce432c470a9a909
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-10-19 23:44:42 +0000

    correção na criação da foto - content_type

--- src/main/resources/db/migration/V3__criar_tabela_foto.sql
+++ src/main/resources/db/migration/V3__criar_tabela_foto.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `foto` (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `content_length` BIGINT NOT NULL,
-    `contentType` VARCHAR(255) NOT NULL,
+    `content_type` VARCHAR(255) NOT NULL,
     `filename` VARCHAR(255) NOT NULL,  
     `url` VARCHAR(255) NOT NULL,  
     PRIMARY KEY (`id`),
