38e2438d3729c83867434b0a46a586f509535500
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-10-24 02:13:13 +0000

    Contratação de diárias (dados do cliente) - refatoração da configuração do spring Security

--- src/main/java/br/com/urubatanpacheco/ediaristas/config/SecurityConfig.java
+++ src/main/java/br/com/urubatanpacheco/ediaristas/config/SecurityConfig.java
@@ -4,21 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import br.com.urubatanpacheco.ediaristas.core.enums.TipoUsuario;
- 
-@Configuration
+
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
    @Autowired
    private UserDetailsService userDetailsService; 
@@ -26,66 +27,110 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
    @Autowired
    private PasswordEncoder passwordEncoder;
 
-   @Value("${br.com.urubatanpacheco.ediaristas.rememberMe.key}")
-   private String rememberMeKey;
+   @Order(1)
+   @Configuration
+   public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+       }   
 
-   @Value("${br.com.urubatanpacheco.ediaristas.rememberMe.validitySeconds}")
-   private int rememberMeValiditySeconds;
-
-   @Override
-   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       auth
-        .userDetailsService(userDetailsService)
-        .passwordEncoder(passwordEncoder);
-
-   } 
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+            .requestMatchers(requestMatcherCustomizer -> 
+                requestMatcherCustomizer
+                    .antMatchers("/api/**","/auth/**")
+            )
+            .authorizeRequests(authorizeRequestsCustomizer ->
+                authorizeRequestsCustomizer
+                    .anyRequest()
+                    .permitAll()
+            )
+            .csrf(csrfCustomizer ->
+                csrfCustomizer
+                    .disable()
+            )
+            .sessionManagement(sessionManagementCustomizer ->
+                sessionManagementCustomizer
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .cors();        
+        } 
         
-        return super.authenticationManagerBean();
+        @Bean
+        @Override
+        public AuthenticationManager authenticationManagerBean() throws Exception {
+            
+            return super.authenticationManagerBean();
+        }        
     }
 
+    @Order(2)
+    @Configuration
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Value("${br.com.urubatanpacheco.ediaristas.rememberMe.key}")
+        private String rememberMeKey;
+
+        @Value("${br.com.urubatanpacheco.ediaristas.rememberMe.validitySeconds}")
+        private int rememberMeValiditySeconds;
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+       }         
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http
+            .requestMatchers(requestMatcherCustomizer -> 
+                requestMatcherCustomizer
+                    .antMatchers("/admin/**")
+            )
+            .authorizeRequests(authorizeRequestsCustomizer ->
+                authorizeRequestsCustomizer
+                    .anyRequest()
+                    .hasAuthority(TipoUsuario.ADMIN.name()) // rotas admin apenas para usuários ADMIN
+            )
+            .formLogin(formLoginCustomizer ->
+                formLoginCustomizer
+                    .loginPage("/admin/login")
+                    .usernameParameter("email")
+                    .passwordParameter("senha")
+                    .defaultSuccessUrl("/admin/servicos") 
+                    .permitAll()                
+            )
+            .logout(logoutCustomizer ->
+                logoutCustomizer
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout","GET"))
+                    .logoutSuccessUrl("/admin/login")
+            )
+            .rememberMe(rememberMeCustomizer ->
+                rememberMeCustomizer
+                    .rememberMeParameter("lembrar-me")
+                    .tokenValiditySeconds(rememberMeValiditySeconds) // dois dias em segundos
+                    .key(rememberMeKey)
+            )
+            .exceptionHandling(exceptionHandlingCustomizer ->
+                exceptionHandlingCustomizer
+                    .accessDeniedPage("/admin/login")
+            );
+        }
 
 
-    @Override
-   protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/uploads/**").permitAll()
-            .antMatchers("/api/**").permitAll()
-            .antMatchers("/auth/**").permitAll()
-            .antMatchers("/admin/**").hasAuthority(TipoUsuario.ADMIN.toString()) // rotas admin apenas para usuários ADMIN
-            .anyRequest().authenticated(); // qualquer outra rota vai ser bloqueada por padrão
-
-        http.formLogin()
-            .loginPage("/admin/login")
-            .usernameParameter("email")
-            .passwordParameter("senha")
-            .defaultSuccessUrl("/admin/servicos") //, true)
-            .permitAll();
-
-        http.logout()
-            .logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout","GET"))
-            .logoutSuccessUrl("/admin/login");
-
-        http.rememberMe()
-            .rememberMeParameter("lembrar-me")
-            .tokenValiditySeconds(rememberMeValiditySeconds) // dois dias em segundos
-            .key(rememberMeKey);
-
-        http.cors();
-        http.csrf()
-        .ignoringAntMatchers("/auth/**")
-        .ignoringAntMatchers("/api/**");
-   }
-
-   @Override
-   public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-           .antMatchers("/webjars/**")
-           .antMatchers("/img/**"); // aonfr pot padrão ficam os arquivos estáticos da aplicação
-
-   }
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web
+            .ignoring()
+            .antMatchers("/webjars/**")
+            .antMatchers("/img/**"); // aonfr pot padrão ficam os arquivos estáticos da aplicação
+        }
+    }
 }
 
