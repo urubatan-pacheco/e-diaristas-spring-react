c8e3de3a02a47af3d6480865827920bfd634cb2f
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-10-29 03:45:54 +0000

    Contratação de diárias (dados do cliente) - atualização do HATEOAS

--- src/main/java/br/com/urubatanpacheco/ediaristas/api/controllers/HomeRestController.java
+++ src/main/java/br/com/urubatanpacheco/ediaristas/api/controllers/HomeRestController.java
@@ -19,34 +19,65 @@ public class HomeRestController {
 
         var listarServicosLink = linkTo(methodOn(ServicoRestController.class)
             .buscarTodos())
-            .withRel("listar_servicos")
-            .withType("GET");
+                .withRel("listar_servicos")
+                .withType("GET");
         
         var enderecoCepLink = linkTo(methodOn(EnderecoRestController.class)
             .buscarEnderecoPorCep(null))
-            .withRel("endereco_cep")
-            .expand()
-            .withType("GET");
+                .withRel("endereco_cep")
+                .expand()
+                .withType("GET");
         
         var diaristasLocalidadeLink = linkTo(methodOn(DiaristaRestController.class)
             .buscarDiaristasPorCep(null))
-            .withRel("diaristas_localidades")
-            .expand()
-            .withType("GET");
+                .withRel("diaristas_localidades")
+                .expand()
+                .withType("GET");
 
         var verificarDisponibilidadeAtendimentoLink = linkTo(methodOn(DiaristaRestController.class)
             .verificarDiaristasPorCep(null))
-            .withRel("verificar_disponibilidade_atendimento")
-            .expand()
-            .withType("GET");            
+                .withRel("verificar_disponibilidade_atendimento")
+                .expand()
+                .withType("GET");
             
+
+        var cadastrarUsuarioLink = linkTo(methodOn(UsuarioRestController.class)
+            .cadastrar(null))
+                .withRel("cadastrar_usuario")
+                .withType("POST");             
+            
+        var loginLink = linkTo(methodOn(AuthRestController.class)
+            .autenticar(null))
+                .withRel("login")
+                .withType("POST");  
+
+        var refreshLink = linkTo(methodOn(AuthRestController.class)
+            .reautenticar(null))
+                .withRel("refresh")
+                .withType("POST");  
+
+        var logoutLink = linkTo(methodOn(AuthRestController.class)
+            .logout(null))
+                .withRel("logout")
+                .withType("POST");  
+
+        var usuarioLogadoLink = linkTo(methodOn(MeRestController.class)
+            .me())
+                .withRel("usuario_logado")
+                .withType("GET");                  
+
         var response = new HateoasResponse();
 
         response.adicionarLinks(
             listarServicosLink, 
             enderecoCepLink, 
             diaristasLocalidadeLink, 
-            verificarDisponibilidadeAtendimentoLink);
+            verificarDisponibilidadeAtendimentoLink,
+            cadastrarUsuarioLink,
+            loginLink,
+            refreshLink,
+            logoutLink,
+            usuarioLogadoLink);
 
         return response;
     }
