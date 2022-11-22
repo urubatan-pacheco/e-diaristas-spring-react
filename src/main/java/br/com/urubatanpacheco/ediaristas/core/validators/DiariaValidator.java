756f2a8dc64cf1dc2d72a5326d62b4049cab5411
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-11-22 02:07:19 +0000

    Contratação de diárias (cadastro de diária) - validando o tempo de atendimento da diária

--- src/main/java/br/com/urubatanpacheco/ediaristas/core/validators/DiariaValidator.java
+++ src/main/java/br/com/urubatanpacheco/ediaristas/core/validators/DiariaValidator.java
@@ -12,6 +12,7 @@ public class DiariaValidator {
 
     public void validar(Diaria diaria) {
         validarHoraTermini(diaria);
+        validarTempoAtendimento(diaria);
     }
 
     private void validarHoraTermini(Diaria diaria) {
@@ -22,7 +23,7 @@ public class DiariaValidator {
         var dataLimite = dataAtendimento.withHour(22).withMinute(0).withSecond(0);
 
         if (dataTermino.isAfter(dataLimite)) {
-            var mensagem = "termino ão pode ser depois das 22h";
+            var mensagem = "o termino não pode ser depois das 22h";
             var fieldError = new FieldError(diaria.getClass().getName(), "dataAtendimento", diaria.getDataAtendimento(), false, null, null, mensagem);
 
             throw new ValidacaoException(mensagem, fieldError);
@@ -30,5 +31,30 @@ public class DiariaValidator {
 
     }
 
+    private void  validarTempoAtendimento(Diaria diaria) {
+        var tempoAtendimento = diaria.getTempoAtendimento();
+        var tempoTotal = calcularTempoTotal(diaria);
+
+        if (tempoAtendimento != tempoTotal) {
+            var mensagem = "tempo de atendimento informado está incorreto";
+            var fieldError = new FieldError(diaria.getClass().getName(), "tempoAtendimento", diaria.getDataAtendimento(), false, null, null, mensagem);
+
+            throw new ValidacaoException(mensagem, fieldError);
+        }        
+
+    }
+
+    private Integer calcularTempoTotal(Diaria diaria) {
+        var servico = diaria.getServico();
+
+        Integer tempoTotal = 0;
+        tempoTotal += diaria.getQuantidadeQuartos() * servico.getHorasQuarto();
+        tempoTotal += diaria.getQuantidadeSalas() * servico.getHorasSala();
+        tempoTotal += diaria.getQuantidadeCozinhas() * servico.getHorasCozinha();
+        tempoTotal += diaria.getQuantidadeBanheiros() * servico.getHorasBanheiro();
+        tempoTotal += diaria.getQuantidadeQuintais() * servico.getHorasQuintal();
+        tempoTotal += diaria.getQuantidadeOutros() * servico.getHorasOutros();
+        return tempoTotal;
+    }
 
 }
