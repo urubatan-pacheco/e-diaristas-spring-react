0e961291ae7b3958114b83eb4f540f63a32317f4
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-11-22 20:21:51 +0000

    Contratação de diárias (cadastro de diária) - validando o preço da diária

--- src/main/java/br/com/urubatanpacheco/ediaristas/core/validators/DiariaValidator.java
+++ src/main/java/br/com/urubatanpacheco/ediaristas/core/validators/DiariaValidator.java
@@ -1,10 +1,13 @@
 package br.com.urubatanpacheco.ediaristas.core.validators;
 
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Component;
 import org.springframework.validation.FieldError;
 
 import br.com.urubatanpacheco.ediaristas.core.exceptions.ValidacaoException;
 import br.com.urubatanpacheco.ediaristas.core.models.Diaria;
+import br.com.urubatanpacheco.ediaristas.core.models.Servico;
 
 
 @Component
@@ -13,7 +16,9 @@ public class DiariaValidator {
     public void validar(Diaria diaria) {
         validarHoraTermini(diaria);
         validarTempoAtendimento(diaria);
+        validarPreco(diaria);
     }
+
 
     private void validarHoraTermini(Diaria diaria) {
         var dataAtendimento = diaria.getDataAtendimento();
@@ -57,4 +62,41 @@ public class DiariaValidator {
         return tempoTotal;
     }
 
+    private void validarPreco(Diaria diaria) {
+        var preco =  diaria.getPreco();
+        var valorTotal = calcularValorTotal(diaria);
+
+        if (preco.compareTo(valorTotal) != 0) {
+            var mensagem = "valor informado está incorreto";
+            var fieldError = new FieldError(diaria.getClass().getName(), "valor", diaria.getDataAtendimento(), false, null, null, mensagem);
+
+            throw new ValidacaoException(mensagem, fieldError);
+        }  
+    }
+
+
+        private BigDecimal calcularValorTotal(Diaria diaria) {
+            var servico = diaria.getServico();
+
+            var valorMinimo = servico.getValorMinimo();
+
+            var valorTotal = calcularValorDoComodo(servico.getValorQuarto(), diaria.getQuantidadeQuartos())
+                .add(calcularValorDoComodo(servico.getValorSala(),diaria.getQuantidadeSalas()))
+                .add(calcularValorDoComodo(servico.getValorCozinha(),diaria.getQuantidadeCozinhas()))
+                .add(calcularValorDoComodo(servico.getValorBanheiro(),diaria.getQuantidadeBanheiros()))
+                .add(calcularValorDoComodo(servico.getValorQuintal(),diaria.getQuantidadeQuintais()))
+                .add(calcularValorDoComodo(servico.getValorOutros(),diaria.getQuantidadeOutros()));
+
+            if (valorTotal.compareTo(valorMinimo) < 0) {
+                valorTotal = valorMinimo;
+            }
+
+            return valorTotal;
+        }
+
+
+        private BigDecimal calcularValorDoComodo(BigDecimal valorComodo, Integer quantidade) {
+            var valor =  valorComodo.multiply(new BigDecimal(quantidade));
+            return valor;
+        }
 }
